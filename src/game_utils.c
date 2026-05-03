5a7a47cd5924f0df1e34e7ccb8ddc88247480443
Tanmay <tanmaypandya159@gmail.com>
2026-05-03 20:47:32 +0000

    Fixed compilation error

--- src/game_utils.c
+++ src/game_utils.c
@@ -11,10 +11,99 @@ void display_welcome(void)
 	ft_printf("Drop your pawn by choosing a column!\n\n");
 }
 
+static int num_width(int n)
+{
+	int width;
+
+	width = 1;
+	while (n >= 10)
+	{
+		n /= 10;
+		width++;
+	}
+	return (width);
+}
+
+static void print_nchar(char ch, int count)
+{
+	while (count-- > 0)
+		ft_printf("%c", ch);
+}
+
+static void print_padded_num(int n, int width)
+{
+	char buf[12];
+	int i;
+	int j;
+	int len;
+	char tmp;
+
+	if (n == 0)
+	{
+		buf[0] = '0';
+		buf[1] = '\0';
+		len = 1;
+	}
+	else
+	{
+		i = 0;
+		while (n > 0 && i < 11)
+		{
+			buf[i++] = (char)('0' + (n % 10));
+			n /= 10;
+		}
+		len = i;
+		j = 0;
+		while (j < len / 2)
+		{
+			tmp = buf[j];
+			buf[j] = buf[len - 1 - j];
+			buf[len - 1 - j] = tmp;
+			j++;
+		}
+		buf[len] = '\0';
+	}
+	print_nchar(' ', width - len);
+	ft_printf("%s", buf);
+}
+
+static void print_header(int row_w, int col_w, int cols)
+{
+	int col;
+
+	print_nchar(' ', row_w);
+	ft_printf(" |" );
+	for (col = 0; col < cols; col++)
+	{
+		print_nchar(' ', 1);
+		print_padded_num(col, col_w);
+		print_nchar(' ', 1);
+		ft_printf("|");
+	}
+	ft_printf("\n");
+}
+
+static void print_border(int row_w, int col_w, int cols)
+{
+	int col;
+
+	print_nchar(' ', row_w);
+	ft_printf(" +");
+	for (col = 0; col < cols; col++)
+	{
+		print_nchar('-', col_w + 2);
+		ft_printf("+");
+	}
+	ft_printf("\n");
+}
+
 void display_board(t_game game)
 {
+	int row_w;
+	int col_w;
 	int row;
 	int col;
+	char token;
 
 	ft_printf("\n");
 
@@ -28,31 +117,32 @@ void display_board(t_game game)
 		return ;
 	}
 
-	for (col = 0; col < game.cols; col++)
-		ft_printf(" %d ", col);
-	ft_printf("\n");
+	row_w = num_width(game.rows - 1);
+	col_w = num_width(game.cols - 1);
 
-	for (col = 0; col < game.cols; col++)
-		ft_printf("---");
-	ft_printf("\n");
+	print_header(row_w, col_w, game.cols);
+	print_border(row_w, col_w, game.cols);
 
 	for (row = 0; row < game.rows; row++)
 	{
+		print_padded_num(row, row_w);
+		ft_printf(" |");
 		for (col = 0; col < game.cols; col++)
 		{
 			if (game.board[row][col] == PLAYER)
-				ft_printf("|X");
+				token = 'X';
 			else if (game.board[row][col] == AI)
-				ft_printf("|O");
+				token = 'O';
 			else
-				ft_printf("| ");
+				token = ' ';
+			ft_printf(" %c", token);
+			print_nchar(' ', col_w - 1);
+			ft_printf(" |");
 		}
-		ft_printf("|\n");
+		ft_printf("\n");
+		print_border(row_w, col_w, game.cols);
 	}
-
-	for (col = 0; col < game.cols; col++)
-		ft_printf("---");
-	ft_printf("\n\n");
+	ft_printf("\n");
 }
 
 int get_player_input(t_game game)
